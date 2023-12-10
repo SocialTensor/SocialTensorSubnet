acc9c6be5df548eea1c305181cb4f45f169826c4
toilaluan <toilaluan@github.com>
2023-12-10 09:35:30 +0000

    update port

--- sd_net/base_miner/miner_api.py
+++ sd_net/base_miner/miner_api.py
@@ -32,4 +32,4 @@ async def get_rewards(data: Prompt):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=10001)
+    uvicorn.run(app, host="0.0.0.0", port=10006)
