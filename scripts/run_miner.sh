f9cd28140b1f1e88939a5716ffa1280064675905
toilaluan <toilaluan@github.com>
2023-12-28 03:34:50 +0000

    add scripts

---
+++ scripts/run_miner.sh
@@ -0,0 +1,6 @@
+python neurons/miner/miner.py \
+--netuid 1 \
+--subtensor.chain_endpoint ws://20.243.203.20:9946 \
+--wallet.name miner --wallet.hotkey default \
+--generate_endpoint http://127.0.0.1:10006/generate \
+--info_endpoint http://127.0.0.1:10006/info
