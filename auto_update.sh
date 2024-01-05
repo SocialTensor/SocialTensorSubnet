ac079f6e4a0cd92c453a10351863d2e8f4df4864
viktorthink <viktor.bowallius@hotmail.com>
2024-01-05 20:57:18 +0000

    fixed auto-updates

--- auto_update.sh
+++ auto_update.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
+
+# Save the current HEAD hash
+current_head=$(git rev-parse HEAD)
+
+# Pull the latest changes from the repository
 git pull
-pm2 restart validator
-while true; do
-    echo "Waiting"
-    sleep 60
-done
+
+# Get the new HEAD hash
+new_head=$(git rev-parse HEAD)
+
+# Check if the new HEAD is different from the current HEAD
+if [ "$current_head" != "$new_head" ]; then
+    # The HEAD has changed, meaning there's a new version
+    echo "New version detected, restarting the validator."
+    pm2 restart validator
+else
+    # No new version, no action needed
+    echo "No new version detected, no restart needed."
+fi
+
+# No need for a loop, as PM2 will execute this script every hour
