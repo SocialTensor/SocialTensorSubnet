74fab12b87d68a6ac14dddbfb7dc952bce800776
toilaluan <toilaluan@github.com>
2024-03-01 09:08:23 +0000

    update

--- services/rewarding/hash_compare.py
+++ services/rewarding/hash_compare.py
@@ -3,35 +3,9 @@ import imagehash
 from PIL import Image, ImageDraw
 from typing import List
 import random
+import asyncio
+from services.rewarding.notice import notice_discord
 from io import BytesIO
-
-def make_image_grid(image_list, grid_size, image_size):
-    """
-    Creates a grid of images and returns a single combined image.
-
-    :param image_list: List of PIL Image objects.
-    :param grid_size: Tuple of (rows, cols) for the grid.
-    :param image_size: Tuple of (width, height) for each image.
-    :return: PIL Image object representing the grid.
-    """
-    rows, cols = grid_size
-    width, height = image_size
-    grid_img = Image.new('RGB', (cols * width, rows * height))
-    
-    for index, img in enumerate(image_list):
-        # Calculate the position of the current image in the grid
-        row = index // cols
-        col = index % cols
-        box = (col * width, row * height, (col + 1) * width, (row + 1) * height)
-        
-        # Resize image if it does not match the specified size
-        if img.size != image_size:
-            img = img.resize(image_size)
-        
-        # Paste the current image into the grid
-        grid_img.paste(img, box)
-        
-    return grid_img
 
 def get_black_hash(H, W) -> str:
     image = Image.new("RGB", (W, H), color="black")
@@ -79,12 +53,7 @@ def infer_hash(validator_image: Image.Image, batched_miner_images: List[str], we
             else:
                 reward = matching_image(miner_image, validator_image)
             if reward <= 0 and webhook and random.random() < probability:
-                notice_image = make_image_grid([validator_image, miner_image], (1, 2), (256, 256))
-                notice_io = BytesIO()
-                notice_image.save(notice_io, format="JPEG")
-                notice_io.seek(0)
-                webhook.add_file(file=notice_io, filename="notice.jpg")
-                webhook.execute()
+                asyncio.create_task(notice_discord(validator_image, miner_image, webhook))
         rewards.append(reward)
     print(rewards, flush=True)
     return rewards
