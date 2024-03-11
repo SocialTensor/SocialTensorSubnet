2f2ab3232c5e4d5258dae2bb08adff6ed12d2b11
toilaluan <toilaluan@github.com>
2024-03-11 08:23:18 +0000

    update

--- image_generation_subnet/validator/offline_reward.py
+++ image_generation_subnet/validator/offline_reward.py
@@ -18,9 +18,9 @@ def get_reward_GoJourney(
     **kwargs,
 ) -> list[float]:
     reward_distribution = {
-        "turbo": 0.14,
-        "fast": 0.07,
-        "relax": 0.03,
+        "turbo": 1.0,
+        "fast": 0.5,
+        "relax": 0.1,
     }
     rewards = []
     prompt = base_synapse.prompt
@@ -29,14 +29,14 @@ def get_reward_GoJourney(
             synapse_response: dict = synapse.response_dict
             bt.logging.info(synapse_response)
             task_id = synapse_response["task_id"]
-            task_request = synapse_response["task_request"]
+            task_request = synapse_response["meta"]["task_request"]
             task_response = fetch_GoJourney(task_id)
             bt.logging.info(task_response)
             task_status = task_response["status"]
             if task_status == "failed" or task_request["prompt"] != prompt:
                 reward = 0
             else:
-                process_mode = task_request["process_mode"]
+                process_mode = task_response["meta"]["task_request"]["process_mode"]
                 reward = reward_distribution[process_mode]
             rewards.append(reward)
         except Exception as e:
