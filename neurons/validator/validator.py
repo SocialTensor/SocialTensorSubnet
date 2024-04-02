faa031b13fcdf03bea75b7eb449bc655fab29aaa
toilaluan <toilaluan@github.com>
2024-04-02 07:27:51 +0000

    update

--- neurons/validator/validator.py
+++ neurons/validator/validator.py
@@ -269,6 +269,7 @@ class Validator(BaseValidatorNeuron):
         loop_base_time = self.config.loop_base_time  # default is 600 seconds
         threads = []
         loop_start = time.time()
+        self.query_queue.update_queue(self.miner_manager.all_uids_info)
         for (
             model_name,
             uids,
