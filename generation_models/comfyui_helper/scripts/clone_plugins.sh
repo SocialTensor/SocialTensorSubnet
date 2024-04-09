6589bdb7a76e5a75fa614d6ccc632b3727f0baf4
toilaluan <toilaluan@github.com>
2024-04-09 08:44:21 +0000

    update

--- generation_models/comfyui_helper/scripts/clone_plugins.sh
+++ generation_models/comfyui_helper/scripts/clone_plugins.sh
@@ -1,5 +1,4 @@
 curl -o /usr/local/bin/pget -L "https://github.com/replicate/pget/releases/download/v0.6.0/pget_linux_x86_64" && chmod +x /usr/local/bin/pget
-pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
 
 cd generation_models/comfyui_helper/ComfyUI/custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager
@@ -16,3 +15,4 @@ git clone https://github.com/cubiq/ComfyUI_essentials && git checkout c9236fe
 
 cd was-node-suite-comfyui
 pip install -r requirements.txt
+pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
