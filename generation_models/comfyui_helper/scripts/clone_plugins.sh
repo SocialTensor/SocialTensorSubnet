6d94e727c5f4c8b312146b5ef93fb7d2ee19cdbd
toilaluan <toilaluan@github.com>
2024-04-09 08:57:06 +0000

    update

--- generation_models/comfyui_helper/scripts/clone_plugins.sh
+++ generation_models/comfyui_helper/scripts/clone_plugins.sh
@@ -15,4 +15,4 @@ git clone https://github.com/cubiq/ComfyUI_essentials && git checkout c9236fe
 
 cd was-node-suite-comfyui
 pip install -r requirements.txt
-pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
+pip install onnxruntime-gpu
