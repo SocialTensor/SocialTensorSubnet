0359ef04c8e4268f92c4356627ada1fcdf53e5b6
toilaluan <toilaluan@github.com>
2024-04-16 05:04:15 +0000

    update

--- services/challenge_generating/llm_prompt_generating/twitter_prompt.py
+++ services/challenge_generating/llm_prompt_generating/twitter_prompt.py
@@ -2,6 +2,8 @@ import typesense
 import os
 import random
 from transformers import AutoTokenizer
+from cleantext import clean
+
 
 TYPESENSE_API_KEY = os.environ.get("TYPESENSE_API_KEY")
 TYPESENSE_HOST = os.environ.get("TYPESENSE_HOST")
@@ -56,6 +58,7 @@ class TwitterPrompt:
             tweets = [tweet["document"] for tweet in tweets]
             tweets = sorted(tweets, key=lambda x: x["likes"], reverse=True)
             texts = [tweet["text"] for tweet in tweets]
+            texts = [self.clean_tweet(tweet) for tweet in texts]
             users = [tweet.get("username", "N/A") for tweet in tweets]
             timestamps = [
                 tweet.get("timestamp", "N/A").split(" ")[0] for tweet in tweets
@@ -93,6 +96,31 @@ class TwitterPrompt:
             num_documents = response["found"]
 
         return num_documents
+
+    def clean_tweet(self, tweet):
+        tweet = clean(
+            tweet,
+            fix_unicode=True,  # fix various unicode errors
+            to_ascii=True,  # transliterate to closest ASCII representation
+            lower=False,  # lowercase text
+            no_line_breaks=False,  # fully strip line breaks as opposed to only normalizing them
+            no_urls=True,  # replace all URLs with a special token
+            no_emails=True,  # replace all email addresses with a special token
+            no_phone_numbers=True,  # replace all phone numbers with a special token
+            no_numbers=False,  # replace all numbers with a special token
+            no_digits=False,  # replace all digits with a special token
+            no_currency_symbols=False,  # replace all currency symbols with a special token
+            no_punct=False,  # remove punctuations
+            replace_with_punct="",  # instead of removing punctuations you may replace them
+            replace_with_url="<URL>",
+            replace_with_email="<EMAIL>",
+            replace_with_phone_number="<PHONE>",
+            replace_with_number="<NUMBER>",
+            replace_with_digit="0",
+            replace_with_currency_symbol="<CUR>",
+            lang="en",  # set to 'de' for German special handling
+        )
+        return tweet
 
 
 if __name__ == "__main__":
