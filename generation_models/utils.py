b2fdaeb18330fd7b82ecd209f6a877ab2eec5bfb
toilaluan <toilaluan@github.com>
2024-04-23 12:19:44 +0000

    update

--- generation_models/utils.py
+++ generation_models/utils.py
@@ -90,7 +90,7 @@ def base64_to_pil_image(base64_image):
 
 def pil_image_to_base64(image: Image.Image) -> str:
     image_stream = io.BytesIO()
-    image.save(image_stream, format="JPEG")
+    image.save(image_stream, format="PNG")
     base64_image = base64.b64encode(image_stream.getvalue()).decode("utf-8")
 
     return base64_image
