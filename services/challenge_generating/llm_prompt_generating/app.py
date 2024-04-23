da9351b950d1f7256013ec5fbc3ea7cd17da565b
toilaluan <toilaluan@github.com>
2024-04-23 16:41:59 +0000

    update

--- services/challenge_generating/llm_prompt_generating/app.py
+++ services/challenge_generating/llm_prompt_generating/app.py
@@ -4,6 +4,7 @@ import uvicorn
 from services.challenge_generating.llm_prompt_generating.twitter_prompt import (
     TwitterPrompt,
 )
+from transformers import AutoTokenizer
 
 
 def get_args():
@@ -23,6 +24,7 @@ def get_args():
 class LLMPromptGenerating:
     def __init__(self):
         self.app = FastAPI()
+        self.tokenizer = AutoTokenizer.from_pretrained("casperhansen/llama-3-70b-instruct-awq")
         self.twitter_prompt = TwitterPrompt(max_tokens=1024)
         self.app.add_api_route("/", self.generate, methods=["POST"])
 
@@ -31,11 +33,18 @@ class LLMPromptGenerating:
             prompt = self.get_twitter_prompt()
         except:
             prompt = "Tell me an event that happened in the history of the world."
+        conversation = [
+            {"role": "user", "content": prompt},
+        ]
+        chat_prompt = self.tokenizer.apply_chat_template(
+            conversation, tokenize=False
+        )
         data = {
-            "prompt_input": prompt,
+            "prompt_input": chat_prompt,
             "pipeline_params": {
                 "max_tokens": 4096,
                 "logprobs": 100,
+                "stop": ["<|eot_id|>"],
             },
         }
         return data
