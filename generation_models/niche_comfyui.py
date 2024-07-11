6cae1af85dc9e94be0ab0bcd8005c8c23b8141c6
vietbeu <vietbeu1998@gmail.com>
2024-07-11 04:59:10 +0000

    clean log comfyui

--- generation_models/niche_comfyui.py
+++ generation_models/niche_comfyui.py
@@ -24,25 +24,25 @@ class NicheComfyUI(BaseModel):
         random_port = random.randint(10000, 50000) # For automatic bind port when scale up using Ray
         self.comfyui = ComfyUI(random_port)
         init_setup_function(self.comfyui, **kwargs)
-        output_folder = f"generation_models/comfyui_helper/ComfyUI/output_{random_port}"
-        input_folder = f"generation_models/comfyui_helper/ComfyUI/input_{random_port}"
-        os.makedirs(output_folder, exist_ok=True)
-        os.makedirs(input_folder, exist_ok=True)
-        self.comfyui.start_server(output_folder.split("/")[-1], input_folder.split("/")[-1])
+        self.output_folder = f"generation_models/comfyui_helper/ComfyUI/output_{random_port}"
+        self.input_folder = f"generation_models/comfyui_helper/ComfyUI/input_{random_port}"
+        os.makedirs(self.output_folder, exist_ok=True)
+        os.makedirs(self.input_folder, exist_ok=True)
+        self.comfyui.start_server(self.output_folder.split("/")[-1], self.input_folder.split("/")[-1])
         workflow = json.load(open(workflow_json_file))
         self.comfyui.load_workflow(workflow)
 
         def inference_function(*args, **kwargs) -> Image.Image:
             self.cleanup(
-                self.comfyui, output_folder, input_folder
+                self.comfyui, self.output_folder, self.input_folder
             )
             workflow = json.load(open(workflow_json_file))
-            update_wf_function(workflow=workflow, input_folder=input_folder, **kwargs)
+            update_wf_function(workflow=workflow, input_folder=self.input_folder, **kwargs)
             wf = self.comfyui.load_workflow(workflow)
             self.comfyui.connect()
             self.comfyui.run_workflow(wf)
             files = []
-            output_directories = [output_folder]
+            output_directories = [self.output_folder]
 
             for directory in output_directories:
                 print(f"Contents of {directory}:")
@@ -81,9 +81,20 @@ class NicheComfyUI(BaseModel):
 
     def __del__(self):
         """Destructor to release resources when deleting the object.
-        Kills the ComfyUI process on its assigned port and deletes all attributes of the instance.
+        Kills the ComfyUI process on its assigned port, clean data, logs and deletes all attributes of the instance.
         """
+        import glob
         self.comfyui.kill_process_on_port()
+        for directory in [self.output_folder, self.input_folder]:
+            print(directory, os.path.exists(directory))
+            if os.path.exists(directory):
+                shutil.rmtree(directory)
+
+        log_file_pattern = os.path.join("generation_models/comfyui_helper/ComfyUI", '*.log')
+        log_files = glob.glob(log_file_pattern)
+        for log_file in log_files:
+            os.remove(log_file)
+
         for attr_name in dir(self):
             attr = getattr(self, attr_name)
             del attr
