6d0701ebc68b916258e2dd6dd7daac5e53f40c43
ViktorThink <35969959+ViktorThink@users.noreply.github.com>
2024-07-20 14:01:21 +0200

    Update weight version

--- logicnet/__init__.py
+++ logicnet/__init__.py
@@ -5,7 +5,7 @@ from . import validator
 from . import miner
 from . import utils
 
-__version__ = "0.1.5"
+__version__ = "1.0.0"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
