d54ddb0112ea0a3c2eafb73294df649c416f0c26
Tran Thanh Luan <92072154+toilaluan@users.noreply.github.com>
2024-07-23 21:49:32 +0700

    Update download_antelopev2.sh

--- generation_models/custom_pipelines/scripts/download_antelopev2.sh
+++ generation_models/custom_pipelines/scripts/download_antelopev2.sh
@@ -1,5 +1,5 @@
 dest_dir=checkpoints/insightface/models/antelopev2
-mkdirs $dest_dir
+mkdir -p $dest_dir
 wget "https://huggingface.co/DIAMONIK7777/antelopev2/resolve/main/1k3d68.onnx?download=true" -P $dest_dir --content-disposition
 wget "https://huggingface.co/DIAMONIK7777/antelopev2/resolve/main/2d106det.onnx?download=true" -P $dest_dir --content-disposition
 wget "https://huggingface.co/DIAMONIK7777/antelopev2/resolve/main/genderage.onnx?download=true" -P $dest_dir --content-disposition
