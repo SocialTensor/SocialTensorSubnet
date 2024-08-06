56c02cac2afedbb6af0e31a8671549903408e6a5
toilaluan <toilaluan@github.com>
2024-08-06 07:01:43 +0000

    Fixes

--- generation_models/niche_supir.py
+++ generation_models/niche_supir.py
@@ -38,9 +38,6 @@ class NicheSUPIR(BaseModel):
             snapshot_download(repo_id="openai/clip-vit-large-patch14", local_dir=path_clip_large)
             hf_hub_download(repo_id="laion/CLIP-ViT-bigG-14-laion2B-39B-b160k", filename="open_clip_pytorch_model.bin", local_dir=path_clip_bigG)
             hf_hub_download(repo_id="camenduru/SUPIR", filename="sd_xl_base_1.0_0.9vae.safetensors", local_dir=path_SDXL_cache)
-
-            
-            hf_hub_download(repo_id="camenduru/SUPIR", filename="SUPIR-v0F.ckpt", local_dir=path_SUPIR_cache)
             hf_hub_download(repo_id="camenduru/SUPIR", filename="SUPIR-v0Q.ckpt", local_dir=path_SUPIR_cache)
 
             hf_hub_download(repo_id="RunDiffusion/Juggernaut-XL-Lightning", filename="Juggernaut_RunDiffusionPhoto2_Lightning_4Steps.safetensors", local_dir=path_SDXL_lightning_cache)
@@ -54,51 +51,16 @@ class NicheSUPIR(BaseModel):
         self.model.to(self.device)
         self.model.first_stage_model.denoise_encoder_s1 = copy.deepcopy(self.model.first_stage_model.denoise_encoder)
         self.model.current_model = 'v0-Q'
-        self.ckpt_Q, self.ckpt_F = load_QF_ckpt(self.opt_file)
-        
+        self.ckpt_Q = load_QF_ckpt(self.opt_file)
+        print('load v0-Q')
+        self.model.load_state_dict(self.ckpt_Q, strict=False)
+        self.model.current_model = 'v0-Q'
         self.config = yaml.load(open(self.opt_file), yaml.FullLoader)
-        
-
-        def stage1_process(
-            input_image,
-            gamma_correction=1.0,
-            diff_dtype="fp16",
-            ae_dtype="bf16",
-            **kwargs
-        ):
-            print('stage1_process ==>>')
-            LQ = HWC3(np.array(Image.open(input_image)))
-            LQ = fix_resize(LQ, 512)
-
-            # stage1
-            LQ = np.array(LQ) / 255 * 2 - 1
-            LQ = torch.tensor(LQ, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0).to(self.device)[:, :3, :, :]
-            self.model.ae_dtype = convert_dtype(ae_dtype)
-            self.model.dtype = convert_dtype(diff_dtype)
-            LQ = self.model.batchify_denoise(LQ, is_stage1=True)
-            LQ = (LQ[0].permute(1, 2, 0) * 127.5 + 127.5).cpu().numpy().round().clip(0, 255).astype(np.uint8)
-
-            # gamma correction
-            LQ = LQ / 255.0
-            LQ = np.power(LQ, gamma_correction)
-            LQ *= 255.0
-            LQ = LQ.round().clip(0, 255).astype(np.uint8)
-            print('<<== stage1_process')
-            return LQ
 
         def stage2_process(input_image, prompt, a_prompt, n_prompt, num_samples, upscale, edm_steps, s_stage1, s_stage2,
                     s_cfg, seed, s_churn, s_noise, color_fix_type, diff_dtype, ae_dtype, gamma_correction,
                     linear_CFG, linear_s_stage2, spt_linear_CFG, spt_linear_s_stage2, model_select, **kwargs):
 
-            if model_select != self.model.current_model:
-                if model_select == 'v0-Q':
-                    print('load v0-Q')
-                    self.model.load_state_dict(self.ckpt_Q, strict=False)
-                    self.model.current_model = 'v0-Q'
-                elif model_select == 'v0-F':
-                    print('load v0-F')
-                    self.model.load_state_dict(self.ckpt_F, strict=False)
-                    self.model.current_model = 'v0-F'
             input_image = HWC3(input_image)
             input_image = upscale_image(input_image, upscale, unit_resolution=32,
                                         max_size=self.max_size)
@@ -133,7 +95,6 @@ class NicheSUPIR(BaseModel):
                 raise ValueError(f"Pipeline type {pipeline_type} is not supported")
             
             input_image = np.array(base64_to_pil_image(kwargs["conditional_image"]))
-            # stage1_result = stage1_process(input_image=input_image, **kwargs)
             
             model_params = self.config["model"]["params"]
             sampler_config = model_params["sampler_config"]["params"]
