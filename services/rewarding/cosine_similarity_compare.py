c71b22f26a53802874bc66aa8baed2f87ba38085
Tran Thanh Luan <92072154+toilaluan@users.noreply.github.com>
2024-08-16 13:01:12 +0700

    update thresholding

--- services/rewarding/cosine_similarity_compare.py
+++ services/rewarding/cosine_similarity_compare.py
@@ -15,7 +15,7 @@ class CosineSimilarityReward(nn.Module):
     def __init__(
         self,
         model_name="vit_base_patch16_clip_384.laion2b_ft_in12k_in1k",
-        threshold=0.6,
+        threshold=0.9,
         device = None
     ):
         super(CosineSimilarityReward, self).__init__()
@@ -47,7 +47,7 @@ class CosineSimilarityReward(nn.Module):
             if cosine_similarity.item() > self.threshold:
                 reward = 1.0
             elif cosine_similarity.item() > 0.4:
-                reward = cosine_similarity.item()
+                reward = (cosine_similarity.item()+0.1)**2
             else:
                 reward = 0.0
             return reward
