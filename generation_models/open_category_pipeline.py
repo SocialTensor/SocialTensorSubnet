dd432dd3f1341f5a96bcb3c32b9fe5036e18d59c
vietbeu <vietbeu1998@gmail.com>
2024-08-29 00:45:54 -0700

    update default pipeline for OC

--- generation_models/open_category_pipeline.py
+++ generation_models/open_category_pipeline.py
@@ -1,16 +1,20 @@
 from PIL import Image
 from .base_model import BaseModel
-
+import diffusers
 class OpenCategoryPipeline(BaseModel):
-    def load_model(self, supporting_pipelines, **kwargs):
-        ### Miner update code here
+    def load_model(self, repo_id, supporting_pipelines, **kwargs):
+        ### Miner update code here. Currently, only the txt2img pipeline is applied, using the default diffusers.DiffusionPipeline. 
+        ### If using the default pipeline, update repo_id in generation_models/configs/model_config.yaml to change model.
+
+        txt2img_pipeline = diffusers.DiffusionPipeline.from_pretrained(repo_id)
+        txt2img_pipeline.to("cuda")
         pipelines = {
-            "txt2img": None
+            "txt2img": txt2img_pipeline
         }
 
         def inference_function(*args, **kwargs) -> Image.Image:
             pipeline_type = kwargs["pipeline_type"]
-            pipeline = pipelines.get("pipeline_type")
+            pipeline = pipelines.get(pipeline_type)
             if not pipeline:
                 raise ValueError(f"Pipeline type {pipeline_type} is not supported")
             
