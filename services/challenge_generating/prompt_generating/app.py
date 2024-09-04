b8ee1511c42b1b49d28cb5a243b83a583b540de2
toilaluan <toilaluan@github.com>
2024-09-04 15:53:06 +0000

    update prompt generation

--- services/challenge_generating/prompt_generating/app.py
+++ services/challenge_generating/prompt_generating/app.py
@@ -3,12 +3,15 @@ from pydantic import BaseModel, Extra
 import argparse
 from typing import Optional
 import uvicorn
-from services.challenge_generating.prompt_generating.model import ChallengePromptGenerator
+import random
+import openai
+
 
 class Prompt(BaseModel, extra=Extra.allow):
     prompt: str
     seed: Optional[int] = 0
     max_length: Optional[int] = 77
+    model_name: Optional[str] = "OpenGeneral"
 
 
 def get_args():
@@ -26,25 +29,59 @@ def get_args():
     return args
 
 
-class ChallengeImage:
+class ChallengePrompt:
     def __init__(self):
-        self.challenge_prompt = ChallengePromptGenerator()
         self.app = FastAPI(title="Challenge Prompt")
         self.app.add_api_route("/", self.__call__, methods=["POST"])
+        self.open_category_prefixes = {
+            "OpenGeneral": [
+                "an image of ",
+                "a landscape image of ",
+                "a painting of ",
+                "a drawing of ",
+                "a portrait photo of ",
+                "an illustration of ",
+                "a animated image of ",
+                "a sketch of ",
+            ],
+            "OpenLandscape": [
+                "an landscape image of",
+                "a landscape photo of",
+                "a landscape view of",
+            ],
+        }
+
+        self.vllm_client = openai.AsyncOpenAI(base_url="http://localhost:8000/v1")
 
     async def __call__(
         self,
-        data: Prompt,
+        payload: Prompt,
     ):
-        data = dict(data)
-        prompt = data["prompt"]
-        if not prompt:
-            prompt = "an image of "
-        complete_prompt = self.challenge_prompt.infer_prompt([prompt], max_generation_length=77, sampling_topk=100)[0].strip()
-        return {"prompt": complete_prompt}
+        model_name = payload.model_name
+        prompt = payload.prompt
+        prompt = self.open_category_prefixes.get(
+            model_name, random.choice(self.open_category_prefixes["OpenGeneral"])
+        )
+        prompt = f"<|endoftext|> {prompt}"
 
-if __name__ == '__main__':
+        output = self.vllm_client.completions.create(
+            model="toilaluan/Image-Caption-Completion-Long",
+            prompt=prompt,
+            max_tokens=125,
+            temperature=0.5,
+            top_p=1,
+            n=1,
+        )
+        completed_prompt = output.choices[0].text.strip()
+        completed_prompt = completed_prompt.replace("\n", " ")
+        completed_prompt = completed_prompt.split(".")[:-1]
+        completed_prompt = ".".join(completed_prompt) + "."
+
+        return {"prompt": completed_prompt}
+
+
+if __name__ == "__main__":
     args = get_args()
-    print("Args: ",args)
-    app = ChallengeImage()
+    print("Args: ", args)
+    app = ChallengePrompt()
     uvicorn.run(app.app, host="0.0.0.0", port=args.port)
