b4b112b81fe954df487376faf4c5113eaf4d2751
root <vietbeu1998@gmail.com>
2024-09-19 18:28:45 +0000

    update format output pixtral

--- services/miner_endpoint/multimodal_app.py
+++ services/miner_endpoint/multimodal_app.py
@@ -66,7 +66,9 @@ class MultiModalGenerating:
 
     async def generate(self, data: dict):
         request = {
-            "model": self.repo_id
+            "model": self.repo_id,
+            "logprobs": True,
+            "top_logprobs": 10
         }
         request.update(data.get("pipeline_params", {}))
         message_content = [
@@ -92,11 +94,10 @@ class MultiModalGenerating:
         ]
         request["messages"] = conversation 
         completion =self.client.chat.completions.create(**request)
-        response = completion.choices[0].message.content
-        print(completion)
+        completion = MultiModalGenerating.convert_chat_completion_ressponse_to_completion_response(completion)
 
         return {
-            "prompt_output": response,
+            "prompt_output": completion,
         }
 
     async def info(self):
@@ -104,6 +105,33 @@ class MultiModalGenerating:
             "model_name": args.model_name,
         }
 
+    @staticmethod
+    def convert_chat_completion_ressponse_to_completion_response(chat_completion):
+        choices = []
+        for ch in chat_completion.choices:
+            text_offset, offset, token_logprobs, tokens, top_logprobs = [], 0, [], [], []
+            for cnt in ch.logprobs.content:
+                text_offset.append(offset)
+                offset += len(cnt.token)
+                token_logprobs.append(cnt.logprob)
+                tokens.append(cnt.token)
+                top_logprobs.append({x.token: x.logprob for x in cnt.top_logprobs})
+            choice = {
+                "index": ch.index,
+                "text": ch.message.content,
+                "logprobs": {
+                    "text_offset": text_offset,
+                    "token_logprobs": token_logprobs,
+                    "tokens": tokens,
+                    "top_logprobs": top_logprobs
+                },
+                
+            }
+            choices.append(choice)
+        chat_completion.choices = choices
+
+        completion = chat_completion.__dict__
+        return completion
 
 if __name__ == "__main__":
     args = get_args()
