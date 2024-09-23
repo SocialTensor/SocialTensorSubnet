9f2b4ee367efa7e6ce79dfc1197660853e7d729c
vietbeu <44457232+vietbeu@users.noreply.github.com>
2024-09-23 14:47:33 +0700

    Add Pixtral (#97)
    * predownload ckpt supir
    
    * add pixtral
    
    * update format output pixtral
    
    * update
    
    * update reward pixtral
    
    * update reqs.txt
    
    ---------
    
    authored-by: vietle

---
+++ services/miner_endpoint/multimodal_app.py
@@ -0,0 +1,119 @@
+import argparse
+from fastapi import FastAPI
+import uvicorn
+from openai import OpenAI
+import httpx
+import yaml
+import copy
+from services.utils import convert_chat_completion_response_to_completion_response
+
+MODEL_CONFIG = yaml.load(
+    open("generation_models/configs/model_config.yaml"), yaml.FullLoader
+)
+
+# Filter out the model names that are not VLM
+MODEL_CONFIG = {
+    k: v
+    for k, v in MODEL_CONFIG.items()
+    if "visual_question_answering" in v["params"]["supporting_pipelines"]
+}
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Run the multi modal endpoint service"
+    )
+    parser.add_argument(
+        "--port", type=int, default=10006, help="Port to run the service on"
+    )
+    parser.add_argument(
+        "--bind_ip", type=str, default="0.0.0.0", help="IP address to run the service on"
+    )
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        choices=MODEL_CONFIG.keys(),
+        default="Pixtral_12b",
+        help="Name of the model to use",
+    )
+    parser.add_argument(
+        "--vllm_url",
+        type=str,
+        default="http://localhost:8000/v1",
+        help="URL of the VLLM service",
+    )
+    parser.add_argument(
+        "--vllm_api_key",
+        type=str,
+        default="apikey",
+        help="api key of the VLLM service",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+class MultiModalGenerating:
+    def __init__(self, args):
+        self.app = FastAPI()
+        self.repo_id = MODEL_CONFIG[args.model_name]["repo_id"]
+        self.args = args
+        self.client = OpenAI(
+            base_url=self.args.vllm_url,
+            api_key=self.args.vllm_api_key,
+        )
+        
+        self.app.add_api_route("/generate", self.generate, methods=["POST"])
+        self.app.add_api_route("/info", self.info, methods=["GET"])
+
+    async def generate(self, data: dict):
+        request = {
+            "model": self.repo_id,
+            "seed": data["seed"]
+        }
+        pipeline_params = data.get("pipeline_params", {})
+        logprobs = pipeline_params.get("logprobs")
+        if logprobs:
+            pipeline_params["top_logprobs"] = copy.deepcopy(pipeline_params["logprobs"])
+            pipeline_params["logprobs"] = True
+        request.update(pipeline_params)
+        message_content = [
+            {
+                "type": "text",
+                "text": data["prompt"][0]
+            }
+        ]
+
+        if data.get("image_url"):
+                message_content.append({
+                "type": "image_url",
+                "image_url": {
+                    "url": data["image_url"]
+                },
+            })
+
+        conversation = [
+            {
+                "role": "user", 
+                "content": message_content
+            },
+        ]
+        request["messages"] = conversation 
+        completion =self.client.chat.completions.create(**request)
+        completion = convert_chat_completion_response_to_completion_response(completion)
+
+        return {
+            "prompt_output": completion,
+        }
+
+    async def info(self):
+        return {
+            "model_name": args.model_name,
+        }
+
+
+if __name__ == "__main__":
+    args = get_args()
+    app = MultiModalGenerating(args)
+
+    uvicorn.run(app.app, host=args.bind_ip, port=args.port)
