2f7e34f0dab42776a7bdbea4487c4e4b22af7a81
toilaluan <toilaluan@github.com>
2024-09-26 05:51:55 +0000

    update

--- image_generation_subnet/miner/constants.py
+++ image_generation_subnet/miner/constants.py
@@ -5,6 +5,9 @@ events {
 }
 
 http {
+    ignore_invalid_headers off;
+    client_max_body_size 0;
+    proxy_intercept_errors on;
     server {
         listen {{external_axon_port}};  # Port to listen on
 
@@ -13,13 +16,11 @@ http {
         # allow 98.76.54.32;   # You can add multiple allowed IPs
         {{whitelist}}
         deny all;            # Deny all other IPs
+        access_log /var/log/nginx/access.log proxylog;
+        proxy_http_version 1.1;
 
         location / {
             proxy_pass http://127.0.0.1:{{internal_axon_port}};
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
         }
     }
 }
